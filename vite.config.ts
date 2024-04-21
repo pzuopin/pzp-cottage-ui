a11cf088571449cb7631c65e235fdf5bfb648e47
pengforfight <pzuopin@163.com>
2024-04-21 19:08:20 +0800

    vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { resolve } from "path";
 import react from '@vitejs/plugin-react'
@@ -27,4 +28,10 @@ export default defineConfig({
 			},
 		},
 	},
+	test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./lib/test/setup.ts",
+    css: true,
+  },
 })
