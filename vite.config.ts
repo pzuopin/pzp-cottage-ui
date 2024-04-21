087ba3aa7f631b180951f828bc38f7a3cb5c09ca
pengforfight <pzuopin@163.com>
2024-04-21 12:06:49 +0800

    step1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+		// library entry and output settings
+		lib: {
+			entry: resolve(__dirname, "lib/main.ts"),
+			name: "pzp-cottage-ui",
+			fileName: "pzp-cottage-ui",
+		},
+		// bundler options
+		// externalize react-related imports
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+					"react/jsx-runtime": "react/jsx-runtime",
+				},
+			},
+		},
+	},
 })
